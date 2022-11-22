9fe833a474bc93528e44636d1a98caa80507a3a6
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2022-11-22 22:44:35 +0800

    Add files via upload

--- trojango/deltrgo.sh
+++ trojango/deltrgo.sh
@@ -13,6 +13,12 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- icanhazip.com);
+sleep 1
+else
+clear
+echo -e "${PURPLE}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
 exit 0
 fi
 clear
@@ -20,17 +26,16 @@ echo "Checking VPS"
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan-go/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		clear
 		echo ""
 		echo "You have no existing clients!"
 		exit 1
 	fi
 
-	clear
 	echo ""
-	echo "Select the existing client you want to renew"
+	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
-	echo -e "==============================="
+	echo " ==============================="
+	echo "     No  Expired   User"
 	grep -E "^### " "/etc/trojan-go/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
@@ -39,22 +44,19 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan-go/akun.conf")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-read -p "Expired (Days) : " masaaktif
+CLIENT_NAME=$(grep -E "^### " "/etc/trojan-go/akun.conf" | cut -d ' ' -f 2-3 | sed -n "${CLIENT_NUMBER}"p)
 user=$(grep -E "^### " "/etc/trojan-go/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/etc/trojan-go/akun.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-now=$(date +%Y-%m-%d)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-exp3=$(($exp2 + $masaaktif))
-exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/trojan-go/akun.conf
+sed -i "/^### $user $exp/d" /etc/trojan-go/akun.conf
+sed -i '/^,"'"$user"'"$/d' /etc/trojan-go/config.json
+systemctl restart trojan-go.service
+service cron restart
 clear
 echo ""
 echo "============================"
-echo "  TrojanGo Account Renewed  "
+echo "  TrojanGo Account Deleted  "
 echo "============================"
 echo "Username : $user"
-echo "Expired  : $exp4"
-echo "=========================="
+echo "Expired  : $exp"
+echo "============================"
 echo "Script By FREE FINDER"
