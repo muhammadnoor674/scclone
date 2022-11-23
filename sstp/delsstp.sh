f22e7318363436328cc9a6fc84d06bfcf2bca94d
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2022-11-23 08:30:32 +0800

    Add files via upload

--- sstp/delsstp.sh
+++ sstp/delsstp.sh
@@ -20,41 +20,35 @@ echo "Checking VPS"
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/akbarstorevpn/data-user-sstp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		clear
 		echo ""
 		echo "You have no existing clients!"
 		exit 1
 	fi
 
-	clear
 	echo ""
-	echo "Select the existing client you want to renew"
+	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
-	echo -e "==============================="
+	echo " ==============================="
+	echo "     No  Expired   User"
 	grep -E "^### " "/var/lib/akbarstorevpn/data-user-sstp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
+			read -rp "Pilih salah satu[1]: " CLIENT_NUMBER
 		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+			read -rp "Pilih salah satu [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-read -p "Expired (Days): " masaaktif
-user=$(grep -E "^### " "/var/lib/akbarstorevpn/data-user-sstp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/var/lib/akbarstorevpn/data-user-sstp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-now=$(date +%Y-%m-%d)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-exp3=$(($exp2 + $masaaktif))
-exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/akbarstorevpn/data-user-sstp
+Client=$(grep -E "^###" /var/lib/akbarstorevpn/data-user-sstp | cut -d ' ' -f 2-3 | sed -n "$CLIENT_NUMBER"p)
+user=$(grep -E "^###" /var/lib/akbarstorevpn/data-user-sstp | cut -d ' ' -f 2 | sed -n "$CLIENT_NUMBER"p)
+exp=$(grep -E "^###" /var/lib/akbarstorevpn/data-user-sstp | cut -d ' ' -f 3 | sed -n "$CLIENT_NUMBER"p)
+sed -i "/^### $user $exp/d" /var/lib/akbarstorevpn/data-user-sstp
+sed -i '/^'"$user"'/d' /home/sstp/sstp_account
 clear
 echo ""
-echo "========================"
-echo "  SSTP Account Renewed  "
-echo "========================"
+echo "=========================="
+echo "   SSTP Account Deleted   "
+echo "=========================="
 echo "Username  : $user"
-echo "Expired   : $exp4"
-echo "========================"
+echo "Expired   : $exp"
+echo "=========================="
 echo "Script By FREE FINDER"
